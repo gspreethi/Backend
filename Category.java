f3e1880b46643af19974e15124e8f54b1357f393
gspreethi <88823470+gspreethi@users.noreply.github.com>
2021-08-21 11:42:53 +0530

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,57 @@
+package com.app.project.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Category {
+
+	@Id
+	private String catId;
+	private String categoryName;
+	private String type;
+	
+	public Category() {
+		super();
+	}
+
+	public Category(String catId, String categoryName, String type) {
+		super();
+		this.catId = catId;
+		this.categoryName = categoryName;
+		this.type = type;
+	}
+
+	public String getCatId() {
+		return catId;
+	}
+
+	public void setCatId(String catId) {
+		this.catId = catId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [catId=" + catId + ", categoryName=" + categoryName + ", type=" + type + "]";
+	}
+	}
